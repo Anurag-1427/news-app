1812df3ffb59f8a7b8eb628154aa515ddd58b8a9
Anurag-1427 <kaushikanurag2001@gmail.com>
2022-03-31 12:31:14 +0530

    emotion react/styled d(pd)encys needs to be npm i

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import NewsCard from '../NewsCard/NewsCard';
-
+import React from "react";
+import NewsCard from "../NewsCard/NewsCard";
+import useStyles from './styles.js';
+import { Grid, Grow, Typography } from "@material-ui/core";
 
 const NewsCards = ({ articles }) => {
+  const classes = useStyles();
   return (
-    <div>
-        {articles.map((articles, i) => (
-            <NewsCard />
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid key={i} item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex '}}>
+            <NewsCard article={article} i={i} />
+          </Grid>
         ))}
-    </div>
-  )
-}
+      </Grid>
+    </Grow>
+  );
+};
 
-export default NewsCards
+export default NewsCards;
